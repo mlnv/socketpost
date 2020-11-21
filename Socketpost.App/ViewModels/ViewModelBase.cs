1c988fdfd9e53f181c3fb5c35bd3389d41ac8d74
Maksym Yemelianov <maxemev@gmail.com>
2020-11-21 20:32:14 +0100

    Add initial files

---
+++ Socketpost.App/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace Socketpost.App.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
