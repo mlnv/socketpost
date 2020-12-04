0070b73d9466e4a9e40c3a37f32acceb41d2de28
Maksym Yemelianov <maxemev@gmail.com>
2020-12-04 20:26:17 +0100

    Organize UI with spaces; Show Disconnect button

--- Socketpost.App/ViewModels/MainWindowViewModel.cs
+++ Socketpost.App/ViewModels/MainWindowViewModel.cs
@@ -14,42 +14,92 @@ namespace Socketpost.App.ViewModels
             set => this.RaiseAndSetIfChanged(ref output, value);
         }
 
+        public string MessageContent
+        {
+            get => messageContent;
+            set => this.RaiseAndSetIfChanged(ref messageContent, value);
+        }
+
+        public bool IsConnected
+        {
+            get => isConnected;
+            set => this.RaiseAndSetIfChanged(ref isConnected, value);
+        }
+
         public string Message { get; set; }
 
-        public IService Service;
+        public IService Service { get; private set; }
 
         private string output;
+        private string messageContent;
+        private bool isConnected;
 
         public void OnConnectCommand()
         {
             if (string.IsNullOrEmpty(Address))
             {
-                Output += "\n Address is empty.";
+                Output += "Address is empty.\n";
                 return;
             }
 
             Service = new WebSocketService();
-            Service.MessageReceived += MessageReceived;
+            SubscribeOnEvents();
 
-            Output += $"\n Connecting to {Address}.";
+            Output += $"Connecting to {Address}.\n";
             Service.Connect(Address);
+        }
+
+        public void OnDisconnectCommand()
+        {
+            Service.Disconnect();
+            UnsubscribeFromEvents();
+            Output += $"Disconnected from {Address}.\n";
         }
 
         public void OnSendMessageCommand()
         {
             if (Service == null)
             {
-                Output += "\n Not connected to server.";
+                Output += "Not connected to server.\n";
                 return;
             }
 
-            Output += $"\n Sending to server: {Message}.";
+            Output += $"Sending to server: {Message}.\n";
             Service.Send(Message);
+        }
+
+        public void OnCopyToMessageForSendingCommand()
+        {
+            Message = MessageContent;
+        }
+
+        private void SubscribeOnEvents()
+        {
+            Service.MessageReceived += MessageReceived;
+            Service.OnConnected += Connected;
+            Service.OnDisconnected += Disconnected;
+        }
+
+        private void UnsubscribeFromEvents()
+        {
+            Service.MessageReceived -= MessageReceived;
+            Service.OnConnected -= Connected;
+            Service.OnDisconnected -= Disconnected;
+        }
+
+        private void Disconnected()
+        {
+            IsConnected = false;
+        }
+
+        private void Connected()
+        {
+            IsConnected = true;
         }
 
         private void MessageReceived(string message)
         {
-             Output += $"\n Message received: {message}.";
+             Output += $"Message received: {message}.\n";
         }
     }
 }
