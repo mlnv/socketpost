6e8176144d65f756db47ba1f1c2693a164dca3d8
Maksym Yemelianov <maxemev@gmail.com>
2020-12-14 22:17:58 +0100

    Use Prism instead of Avalonia
    Implement selection of messages

---
+++ Socketpost.WinApp/Models/Message.cs
@@ -0,0 +1,9 @@
+﻿namespace Socketpost.WinApp.Models
+{
+    public class Message
+    {
+        public bool Informational { get; set; }
+        public bool FromServer { get; set; }
+        public string Data { get; set; }
+    }
+}
