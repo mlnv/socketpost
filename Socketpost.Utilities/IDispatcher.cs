fe23ceabc35823a4f4e538c906b4d28d589887bb
Maksym Yemelianov <maxemev@gmail.com>
2020-12-26 19:17:59 +0100

    Abstract the way of using dispatcher in the WinApp project, so it can be mocked and used in tests

---
+++ Socketpost.Utilities/IDispatcher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Socketpost.Utilities
+{
+    /// <summary>
+    /// Provides services for managing the queue of work items for a thread
+    /// </summary>
+    public interface IDispatcher
+    {
+        /// <summary>
+        /// Executes the specified action synchronously on the thread the Dispatcher is associated with.
+        /// </summary>
+        /// <param name="method">A delegate to invoke through the dispatcher</param>
+        /// <param name="args">The data to provide</param>
+        void Dispatch(Action method, params object[] args);
+    }
+}
---
+++ Socketpost.WinApp.Tests/MockDispatcher.cs
@@ -0,0 +1,13 @@
+﻿using Socketpost.Utilities;
+using System;
+
+namespace Socketpost.WinApp.Tests
+{
+    internal class MockDispatcher : IDispatcher
+    {
+        public void Dispatch(Action method, params object[] args)
+        {
+            method.DynamicInvoke(args);
+        }
+    }
+}
